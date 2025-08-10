98efe0a7beb3bf713039327d80464e075a4f655a
TiaWidi <tia.septianawidi@gmail.com>
2025-08-10 19:05:28 +0700

    Create Main Application Entry Point

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import * as bootstrap from "bootstrap";
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import AppRoutes from "./routes";
+import { CartProvider } from "./contexts/CartContext";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("root");
+  const root = createRoot(container);
+
+  root.render(
+    <React.StrictMode>
+      <CartProvider>
+        <AppRoutes />
+      </CartProvider>
+    </React.StrictMode>
+  );
+});
