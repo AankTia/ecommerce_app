a39414423120e7ba97f4c2b4cf75d612abf26281
TiaWidi <tia.septianawidi@gmail.com>
2025-08-10 19:27:53 +0700

    Add search functionality for products controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,6 +5,25 @@ module Api::V1
 
     def index
       @products = Product.all
+
+      if params[:search].present?
+        @products = @products.where("name LIKE ? OR description LIKE ?",
+                                  "%#{params[:search]}%",
+                                  "%#{params[:search]}%")
+      end
+
+      if params[:category_id].present?
+        @products = @products.where(category_id: params[:category_id])
+      end
+
+      if params[:min_price].present?
+        @products = @products.where("price >= ?", params[:min_price])
+      end
+
+      if params[:max_price].present?
+        @products = @products.where("price <= ?", params[:max_price])
+      end
+
       render json: @products
     end
 
